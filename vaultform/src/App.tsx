405c62c3b4f115e25ed928b4ca6fcad63787d12d
Ishan Sharma <ish.sharma@mail.utoronto.ca>
2024-02-01 18:02:38 -0500

    Add styling to UI components

--- vaultform/src/App.tsx
+++ vaultform/src/App.tsx
@@ -1,10 +1,22 @@
 import './App.css';
 import { Form } from './components/Form';
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+
+const theme = extendTheme({
+  styles: {
+    global: {
+      body: {
+        bg: 'gray.100', // Set the global background color
+        fontFamily: 'Inter, sans-serif', // Set the default font family
+      },
+    },
+  },
+  // You can extend the theme further to include custom colors, fonts, etc.
+});
 
 function App() {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <div className="App">
         <header className="App-header">
           <Form />
--- vaultform/src/components/Form.tsx
+++ vaultform/src/components/Form.tsx
@@ -1,5 +1,7 @@
 import React, { useEffect } from 'react';
-import { FormControl, FormLabel, Input, FormErrorMessage, Button, VStack } from '@chakra-ui/react';
+import { 
+  Box, FormControl, FormLabel, Input, FormErrorMessage, Button, Heading, Text, Flex
+} from '@chakra-ui/react';
 import { useForm } from 'react-hook-form';
 import useCorporationNumberValidation from '../hooks/useValidation';
 import useSubmit from '../hooks/useSubmit';
@@ -34,28 +36,52 @@ export const Form: React.FC = () => {
   };
 
   return (
-    <VStack spacing={4} as="form" onSubmit={handleSubmit(onFormSubmit)}>
-      <FormControl isInvalid={!!errors.firstName}>
-        <FormLabel htmlFor="firstName">First Name</FormLabel>
-        <Input id="firstName" {...register("firstName", { required: "First Name is required", maxLength: 50 })} />
-        <FormErrorMessage>{errors.firstName?.message}</FormErrorMessage>
-      </FormControl>
-      <FormControl isInvalid={!!errors.lastName}>
-        <FormLabel htmlFor="lastName">Last Name</FormLabel>
-        <Input id="lastName" {...register("lastName", { required: "Last Name is required", maxLength: 50 })} />
-        <FormErrorMessage>{errors.lastName?.message}</FormErrorMessage>
-      </FormControl>
-      <FormControl isInvalid={!!errors.phone}>
-        <FormLabel htmlFor="phone">Phone Number</FormLabel>
-        <Input id="phone" {...register("phone", { required: "Phone is required", pattern: { value: phoneRegex, message: "Invalid Canadian phone number. Format: +1XXXXXXXXXX" } })} />
-        <FormErrorMessage>{errors.phone?.message}</FormErrorMessage>
-      </FormControl>
-      <FormControl isInvalid={!!errors.corporationNumber}>
-        <FormLabel htmlFor="corporationNumber">Corporation Number</FormLabel>
-        <Input id="corporationNumber" {...register("corporationNumber", { required: "Corporation Number is required" })} />
-        {touchedFields.corporationNumber && <FormErrorMessage>{errors.corporationNumber?.message}</FormErrorMessage>}
-      </FormControl>
-      <Button type="submit" colorScheme="blue">Submit</Button>
-    </VStack>
+    <Box
+      bg="white"
+      p={8}
+      rounded="lg"
+      shadow="md"
+      maxW="md"
+      mx="auto"
+      my={10}
+    >
+      <Box display="flex" justifyContent="space-between" mb={5}>
+        <Heading as="h1" color="gray.800" size="lg">Onboarding Form</Heading>
+        <Text fontSize="md" color="gray.600">Step 1 of 5</Text>
+      </Box>
+      <form onSubmit={handleSubmit(onFormSubmit)}>
+        <Flex direction={{ base: 'column', sm: 'row' }} gap={4}>
+          <FormControl isInvalid={!!errors.firstName} flex={1}>
+            <FormLabel htmlFor="firstName" color="gray.700">First Name</FormLabel>
+            <Input id="firstName" color="gray.800" {...register("firstName", { required: "First Name is required", maxLength: 50 })} />
+            <FormErrorMessage>{errors.firstName?.message}</FormErrorMessage>
+          </FormControl>
+          <FormControl isInvalid={!!errors.lastName} flex={1}>
+            <FormLabel htmlFor="lastName" color="gray.700">Last Name</FormLabel>
+            <Input id="lastName" color="gray.800" {...register("lastName", { required: "Last Name is required", maxLength: 50 })} />
+            <FormErrorMessage>{errors.lastName?.message}</FormErrorMessage>
+          </FormControl>
+        </Flex>
+        <FormControl isInvalid={!!errors.phone} mt={4}>
+          <FormLabel htmlFor="phone" color="gray.700">Phone Number</FormLabel>
+          <Input id="phone" color="gray.800" {...register("phone", { required: "Phone is required", pattern: { value: phoneRegex, message: "Invalid phone number. Format: +1XXXXXXXXXX" } })} />
+          <FormErrorMessage>{errors.phone?.message}</FormErrorMessage>
+        </FormControl>
+        <FormControl isInvalid={!!errors.corporationNumber} mt={4}>
+          <FormLabel htmlFor="corporationNumber" color="gray.700">Corporation Number</FormLabel>
+          <Input id="corporationNumber" color="gray.800" {...register("corporationNumber", { required: "Corporation Number is required" })} />
+          <FormErrorMessage>{errors.corporationNumber?.message}</FormErrorMessage>
+        </FormControl>
+        <Button
+          mt={4}
+          type="submit"
+          bg="black"
+          color="white"
+          width="full"
+        >
+          Submit
+        </Button>
+      </form>
+    </Box>
   );
 };
